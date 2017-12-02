2b8984493192af744a60beafa3c408db852e2864
tayyabafzal95 <tayyabafzal95@gmail.com>
2017-12-02 12:36:15 +0500

    new feature

---
+++ resources/views/features/views/hosue_feature.blade.php
@@ -0,0 +1,156 @@
+<section id="features">
+    <div class="title">
+        <h2>Features</h2>
+        <aside class="step">4</aside>
+    </div>
+
+
+    <div class="row">
+        <div class="col-md-4">
+            <h3>Main Features</h3>
+            <ul class="checkboxes inline half list-unstyled">
+                
+                <li><label><input type="text" name="house_Built_in_Year">Built in Year</label></li>
+                <li><label><input type="text" name="house_View">View</label></li>
+                <li><label><input type="text" name="house_Parking_Spaces">Parking Spaces</label></li>
+                
+                <li><label><input type="checkbox" name="house_Double_Glazed_Windows">Double Glazed Windows</label></li>
+                <li><label><input type="checkbox" name="house_Central_Air_Conditioning">Central Air Conditioning</label></li>
+                <li><label><input type="checkbox" name="house_Central_Heating">Central Heating</label></li>
+                
+                <label>Flooring</label>
+                <select>
+                <option value="house_Flooring_Tiles">Tiles</option>
+                <option value="house_Flooring_Marble">Marble</option>
+                <option value="house_Flooring_Wooden">Wooden</option>
+                <option value="house_Flooring_Chip">Chip</option>
+                <option value="house_Flooring_Cement">Cement</option>
+                <option value="house_Flooring_Other">Other</option>
+                </select>
+                
+                <label>Electricity Backup</label>
+                <select>
+                <option value="house_Electricity_Backup_None">None</option>
+                <option value="house_Electricity_Backup_Generator">Generator</option>
+                <option value="house_Electricity_Backup_Ups">Ups</option>
+                <option value="house_Electricity_Backup_Solar">Solar</option>
+                <option value="house_Electricity_Backup_Other">Other</option>
+                </select>
+                
+                <li><label><input type="checkbox" name="house_Waste_Disposal">Waste Disposal</label></li>
+                
+                <li><label><input type="text" name="house_Total_Number_of_Floors">Total Number of Floors</label></li>
+                
+                <li><label><input type="text" name="house_Other_Main_Features">Other Main Features</label></li>
+                
+                <li><label><input type="checkbox" name="house_Furnished">Furnished</label></li>
+                
+
+            </ul>
+            <!--end checkboxes-->
+        </div>
+        <!--end col-md-4-->
+
+
+        <div class="col-md-4">
+            <h3>Buisness and Communication</h3>
+            <ul class="checkboxes inline half list-unstyled">
+                
+                <li><label><input type="checkbox" name="house_Broadband_Internet_Access">Broadband Internet Access</label></li>
+                <li><label><input type="checkbox" name="house_Satellite_or_Cable_TV_Ready">Satellite or Cable TV Ready</label></li>
+                <li><label><input type="chcekbox" name="house_Intercom">Intercom</label></li>
+
+                <li><label><input type="text" name="house_Other_Business_and_Communication_Facilities">Other Business and Communication Facilities</label></li>
+
+            </ul>
+            <!--end checkboxes-->
+        </div>
+        <!--end col-md-4-->
+
+
+        <div class="col-md-4">
+            <h3>Nearby Locations and Other Facilities</h3>
+            <ul class="checkboxes inline half list-unstyled">
+                
+               <li><label><input type="text" name="house_Nearby_Schools">Nearby Schools</label></li>
+                <li><label><input type="text" name="house_Nearby_Hospitals">Nearby Hospitals</label></li>
+                <li><label><input type="text" name="house_Nearby_Shopping_Malls">Nearby Shopping Malls</label></li>
+                <li><label><input type="text" name="house_Nearby_Restaurants">Nearby Restaurants</label></li>
+                <li><label><input type="text" name="house_Distance_From_Airport(kms)">Distance From Airport(kms)</label></li>
+                <li><label><input type="text" name="house_Nearby_Public_Transport_Service">Nearby Public Transport Service</label></li>
+                <li><label><input type="text" name="house_Other_Nearby_Places">Other Nearby Places</label></li>
+           
+            </ul>
+            <!--end checkboxes-->
+        </div>
+        <!--end col-md-4-->
+    </div>
+    <!--end row-->
+
+
+    <div class="row">
+        <div class="col-md-4">
+            <h3>Rooms</h3>
+            <ul class="checkboxes inline half list-unstyled">
+               
+                <li><label><input type="text" name="house_Number_of_Bedrooms">Number of Bedrooms</label></li>
+                <li><label><input type="text" name="house_Number_of_Bathrooms">Number of Bathrooms</label></li>
+                <li><label><input type="text" name="house_Number_of_Servant_Quarters">Number of Servant Quarters</label></li>
+                
+                <li><label><input type="checkbox" name="house_Drawing_Room">Drawing Room</label></li>
+                <li><label><input type="checkbox" name="house_Dining_Room">Dining Room</label></li>
+                
+                <li><label><input type="text" name="house_Number_of_Kitchens">Number of Kitchens</label></li>
+                
+                <li><label><input type="checkbox" name="house_Study_Room">Study Room</label></li>
+                <li><label><input type="checkbox" name="house_Prayer_Room">Prayer Room</label></li>
+                <li><label><input type="checkbox" name="house_Powder_Room">Powder Room</label></li>
+                <li><label><input type="checkbox" name="house_Gym_Room">Gym Room</label></li>
+                
+                <li><label><input type="text" name="house_Number_of_Store_Rooms">Number of Store Rooms</label></li>
+                
+                <li><label><input type="checkbox" name="house_Steaming_Room">Steaming Room</label></li>
+                <li><label><input type="checkbox" name="house_Lounge_or_Sitting_Room">Lounge or Sitting Room</label></li>
+                <li><label><input type="checkbox" name="house_Laundry_Room">Laundry Room</label></li>
+                
+                <li><label><input type="text" name="house_Other_Rooms">Other Rooms</label></li>
+                
+            </ul>
+            <!--end checkboxes-->
+        </div>
+        <!--end col-md-4-->
+
+
+        <div class="col-md-4">
+            <h3>Healthcare Recreational</h3>
+            <ul class="checkboxes inline half list-unstyled">
+                
+                <li><label><input type="checkbox" name="house_Lawn_or_Garden">Lawn or Garden</label></li>
+                <li><label><input type="checkbox" name="house_Swimming_Pool">Swimming Pool</label></li>
+                <li><label><input type="checkbox" name="house_Sauna">Sauna</label></li>
+                <li><label><input type="checkbox" name="house_Jacuzzi">Jacuzzi</label></li>
+                
+                <li><label><input type="text" name="house_Other_Healthcare_and_Recreation_Facilities">Other Healthcare and Recreation Facilities</label></li>
+                
+            </ul>
+            <!--end checkboxes-->
+        </div>
+        <!--end col-md-4-->
+
+
+        <div class="col-md-4">
+            <h3>Other Facilities</h3>
+            <ul class="checkboxes inline half list-unstyled">
+                <li><label><input type="checkbox" name="house_Maintenance_Staff">Maintenance Staff</label></li>
+                <li><label><input type="checkbox" name="house_Security_Staff">Security Staff</label></li>
+                <li><label><input type="checkbox" name="house_Facilities_for_Disabled">Facilities for Disabled</label></li>
+                
+                <li><label><input type="text" name="house_Other_Facilities">Other Facilities</label></li>
+ 
+            </ul>
+            <!--end checkboxes-->
+        </div>
+        <!--end col-md-4-->
+    </div>
+    <!--end row-->
+</section>
