6c42636005aefc6b4e1376f3a4037bead33de68c
tayyabafzal95 <tayyabafzal95@gmail.com>
2017-12-04 16:01:49 +0500

    updatee

--- resources/views/features/building_feature.blade.php
+++ resources/views/features/building_feature.blade.php
@@ -9,44 +9,44 @@
             <ul class="checkboxes inline half list-unstyled">
                 
                 
-                <li><label><input type="text" name="building_Built_in_Year">Built in Year</label></li>
-                <li><label><input type="text" name="building_Parking_Spaces">Parking Spaces</label></li>
+                <li><label><input type="text" name="Built_in_Year">Built in Year</label></li>
+                <li><label><input type="text" name="Parking_Spaces">Parking Spaces</label></li>
                
-                <li><label><input type="checkbox" name="building_Public_Parking">Public Parking</label></li>
-                <li><label><input type="checkbox" name="building_Underground_Parking">Underground Parking</label></li>
-                <li><label><input type="checkbox" name="building_Lobby_in_Building">Lobby in Building</label></li>
-                <li><label><input type="checkbox" name="building_Central_Air_Conditioning">Central Air Conditioning</label></li>
-                <li><label><input type="checkbox" name="building_Central_Heating">Central Heating</label></li>
+                <li><label><input type="checkbox" name="Public_Parking">Public Parking</label></li>
+                <li><label><input type="checkbox" name="Underground_Parking">Underground Parking</label></li>
+                <li><label><input type="checkbox" name="Lobby_in_Building">Lobby in Building</label></li>
+                <li><label><input type="checkbox" name="Central_Air_Conditioning">Central Air Conditioning</label></li>
+                <li><label><input type="checkbox" name="Central_Heating">Central Heating</label></li>
                 
                 <label>Flooring</label>
                 <select>
-                <option value="building_Flooring_Tiles">Tiles</option>
-                <option value="building_Flooring_Marble">Marble</option>
-                <option value="building_Flooring_Wooden">Wooden</option>
-                <option value="building_Flooring_Chip">Chip</option>
-                <option value="building_Flooring_Cement">Cement</option>
-                <option value="building_Flooring_Other">Other</option>
+                <option value="Flooring_Tiles">Tiles</option>
+                <option value="Flooring_Marble">Marble</option>
+                <option value="Flooring_Wooden">Wooden</option>
+                <option value="Flooring_Chip">Chip</option>
+                <option value="Flooring_Cement">Cement</option>
+                <option value="Flooring_Other">Other</option>
                 </select>
                 
                 <label>Electricity Backup</label>
                 <select>
-                <option value="building_Electricity_Backup_None">None</option>
-                <option value="building_Electricity_Backup_Generator">Generator</option>
-                <option value="building_Electricity_Backup_Ups">Ups</option>
-                <option value="building_Electricity_Backup_Solar">Solar</option>
-                <option value="building_Electricity_Backup_Other">Other</option>
+                <option value="Electricity_Backup_None">None</option>
+                <option value="Electricity_Backup_Generator">Generator</option>
+                <option value="Electricity_Backup_Ups">Ups</option>
+                <option value="Electricity_Backup_Solar">Solar</option>
+                <option value="Electricity_Backup_Other">Other</option>
                 </select>
                 
-                <li><label><input type="checkbox" name="building_Waste_Disposal">Waste Disposal</label></li>
+                <li><label><input type="checkbox" name="Waste_Disposal">Waste Disposal</label></li>
                 
-                <li><label><input type="text" name="building_Total_Floors_in_Building">Total Floors in Building</label></li>
+                <li><label><input type="text" name="Total_Floors_in_Building">Total Floors in Building</label></li>
                 
-                <li><label><input type="checkbox" name="building_Elevator_or_Lift">Elevator or Lift</label></li>
+                <li><label><input type="checkbox" name="Elevator_or_Lift">Elevator or Lift</label></li>
                 
-                <li><label><input type="text" name="building_Number_of_Elevators_in_Building">Number of Elevators in Building</label></li>
-                <li><label><input type="checkbox" name="building_Service_Elevators_in_Building">Service Elevators in Building</label></li>
-                <li><label><input type="text" name="building_Other_Main_Features">Other Main Features</label></li>
-                <li><label><input type="text" name="building_Number_of_Units">Number of Units</label></li>
+                <li><label><input type="text" name="Number_of_Elevators_in_Building">Number of Elevators in Building</label></li>
+                <li><label><input type="checkbox" name="Service_Elevators_in_Building">Service Elevators in Building</label></li>
+                <li><label><input type="text" name="Other_Main_Features">Other Main Features</label></li>
+                <li><label><input type="text" name="Number_of_Units">Number of Units</label></li>
                 
                 
 
@@ -57,13 +57,13 @@
         <div class="col-md-4">
             <h3>Buisness and Communication</h3>
             <ul class="checkboxes inline half list-unstyled">
-                <li><label><input type="checkbox" name="building_Broadband_Internet_Access">Broadband Internet Access</label></li>
-                <li><label><input type="checkbox" name="building_Satellite_or_TV_Cable_Ready">Satellite or TV Cable Ready</label></li>
-                <li><label><input type="checkbox" name="building_Business_Center_or_Media_Room_in_Building">Business Center or Media Room in Building</label></li>
-                <li><label><input type="checkbox" name="building_Conference_Room_in_Building">Conference Room in Building</label></li>
-                <li><label><input type="checkbox" name="building_Intercom">Intercom</label></li>
-                <li><label><input type="checkbox" name="building_ATM_Credit_Card_Machines_in_Building">ATM Credit Card Machines in Building</label></li>
-                <li><label><input type="text" name="building_Other_Business_and_Communication_Facilities">Other Business and Communication Facilities</label></li>
+                <li><label><input type="checkbox" name="Broadband_Internet_Access">Broadband Internet Access</label></li>
+                <li><label><input type="checkbox" name="Satellite_or_TV_Cable_Ready">Satellite or TV Cable Ready</label></li>
+                <li><label><input type="checkbox" name="Business_Center_or_Media_Room_in_Building">Business Center or Media Room in Building</label></li>
+                <li><label><input type="checkbox" name="Conference_Room_in_Building">Conference Room in Building</label></li>
+                <li><label><input type="checkbox" name="Intercom">Intercom</label></li>
+                <li><label><input type="checkbox" name="ATM_Credit_Card_Machines_in_Building">ATM Credit Card Machines in Building</label></li>
+                <li><label><input type="text" name="Other_Business_and_Communication_Facilities">Other Business and Communication Facilities</label></li>
                 
                
             </ul>
@@ -73,13 +73,13 @@
         <div class="col-md-4">
             <h3>Nearby Locations and Other Facilities</h3>
             <ul class="checkboxes inline half list-unstyled">
-                <li><label><input type="text" name="building_Nearby_Schools">Nearby Schools</label></li>
-                <li><label><input type="text" name="building_Nearby_Hospitals">Nearby Hospitals</label></li>
-                <li><label><input type="text" name="building_Nearby_Shopping_Malls">Nearby Shopping Malls</label></li>
-                <li><label><input type="text" name="building_Nearby_Restaurants">Nearby Restaurants</label></li>
-                <li><label><input type="text" name="building_Distance_From_Airport(kms)">Distance From Airport(kms)</label></li>
-                <li><label><input type="text" name="building_Nearby_Public_Transport_Service">Nearby Public Transport Service</label></li>
-                <li><label><input type="text" name="building_Other_Nearby_Places">Other Nearby Places</label></li>
+                <li><label><input type="text" name="Nearby_Schools">Nearby Schools</label></li>
+                <li><label><input type="text" name="Nearby_Hospitals">Nearby Hospitals</label></li>
+                <li><label><input type="text" name="Nearby_Shopping_Malls">Nearby Shopping Malls</label></li>
+                <li><label><input type="text" name="Nearby_Restaurants">Nearby Restaurants</label></li>
+                <li><label><input type="text" name="Distance_From_Airport(kms)">Distance From Airport(kms)</label></li>
+                <li><label><input type="text" name="Nearby_Public_Transport_Service">Nearby Public Transport Service</label></li>
+                <li><label><input type="text" name="Other_Nearby_Places">Other Nearby Places</label></li>
 
             </ul>
             <!--end checkboxes-->
@@ -94,18 +94,18 @@
         <div class="col-md-4">
             <h3>Other Facilities</h3>
             <ul class="checkboxes inline half list-unstyled">
-                <li><label><input type="checkbox" name="building_Maintenance_Staff">Maintenance Staff</label></li>
-                <li><label><input type="checkbox" name="building_Security_Staff">Security Staff</label></li>
-                <li><label><input type="checkbox" name="building_Laundry_or_Dry_Cleaning_Facility">Laundry or Dry Cleaning Facility</label></li>
-                <li><label><input type="checkbox" name="building_Facilities_for_Disabled">Facilities for Disabled</label></li>
+                <li><label><input type="checkbox" name="Maintenance_Staff">Maintenance Staff</label></li>
+                <li><label><input type="checkbox" name="Security_Staff">Security Staff</label></li>
+                <li><label><input type="checkbox" name="Laundry_or_Dry_Cleaning_Facility">Laundry or Dry Cleaning Facility</label></li>
+                <li><label><input type="checkbox" name="Facilities_for_Disabled">Facilities for Disabled</label></li>
                 
                 <label>Pet Policy</label>
                 <select>
-                <option value="building_Pet_Policy_Allowed">Allowed</option>
-                <option value="building_Pet_Policy_Not_Allowed">Not Allowed</option>
+                <option value="Pet_Policy_Allowed">Allowed</option>
+                <option value="Pet_Policy_Not_Allowed">Not Allowed</option>
                 </select>
                 
-                <li><label><input type="text" name="building_Other_Facilities">Other Facilities</label></li>
+                <li><label><input type="text" name="Other_Facilities">Other Facilities</label></li>
               
             </ul>
             <!--end checkboxes-->
