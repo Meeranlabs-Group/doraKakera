738c5a2e074922beee9ee69a58375c1cae2055dc
tayyabafzal95 <tayyabafzal95@gmail.com>
2017-12-04 16:26:58 +0500

    updatee

--- resources/views/features/shop_feature.blade.php
+++ resources/views/features/shop_feature.blade.php
@@ -8,42 +8,42 @@
             <h3>Main Features</h3>
             <ul class="checkboxes inline half list-unstyled">
                 
-                <li><label><input type="text" name="shop_Built_in_Year">Built in Year</label></li>
-                <li><label><input type="text" name="shop_View">View</label></li>
-                <li><label><input type="text" name="shop_Parking_Spaces">Parking Spaces</label></li>
+                <li><label><input type="text" name="Built_in_Year">Built in Year</label></li>
+                <li><label><input type="text" name="View">View</label></li>
+                <li><label><input type="text" name="Parking_Spaces">Parking Spaces</label></li>
  
-                <li><label><input type="checkbox" name="shop_Central_Air_Conditioning">Central Air Conditioning</label></li>
-                <li><label><input type="checkbox" name="shop_Central_Heating">Central Heating</label></li>
+                <li><label><input type="checkbox" name="Central_Air_Conditioning">Central Air Conditioning</label></li>
+                <li><label><input type="checkbox" name="Central_Heating">Central Heating</label></li>
                 
                 <label>Flooring</label>
                 <select>
-                <option value="shop_Flooring_Tiles">Tiles</option>
-                <option value="shop_Flooring_Marble">Marble</option>
-                <option value="shop_Flooring_Wooden">Wooden</option>
-                <option value="shop_Flooring_Chip">Chip</option>
-                <option value="shop_Flooring_Cement">Cement</option>
-                <option value="shop_Flooring_Other">Other</option>
+                <option value="Flooring_Tiles">Tiles</option>
+                <option value="Flooring_Marble">Marble</option>
+                <option value="Flooring_Wooden">Wooden</option>
+                <option value="Flooring_Chip">Chip</option>
+                <option value="Flooring_Cement">Cement</option>
+                <option value="Flooring_Other">Other</option>
                 </select>
                 
                 <label>Electricity Backup</label>
                 <select>
-                <option value="shop_Electricity_Backup_None">None</option>
-                <option value="shop_Electricity_Backup_Generator">Generator</option>
-                <option value="shop_Electricity_Backup_Ups">Ups</option>
-                <option value="shop_Electricity_Backup_Solar">Solar</option>
-                <option value="shop_Electricity_Backup_Other">Other</option>
+                <option value="Electricity_Backup_None">None</option>
+                <option value="Electricity_Backup_Generator">Generator</option>
+                <option value="Electricity_Backup_Ups">Ups</option>
+                <option value="Electricity_Backup_Solar">Solar</option>
+                <option value="Electricity_Backup_Other">Other</option>
                 </select>
                 
                 
-               <li><label><input type="checkbox" name="shop_Waste_Disposal">Waste Disposal</label></li>
+               <li><label><input type="checkbox" name="Waste_Disposal">Waste Disposal</label></li>
                 
-                <li><label><input type="text" name="shop_Floor">Floor</label></li>
-                <li><label><input type="text" name="shop_Total_Floors_in_Building">Total Floors in Building</label></li>
-                <li><label><input type="text" name="shop_Number_of_Elevators_in_Building">Number of Elevators in Building</label></li>
+                <li><label><input type="text" name="Floor">Floor</label></li>
+                <li><label><input type="text" name="Total_Floors_in_Building">Total Floors in Building</label></li>
+                <li><label><input type="text" name="Number_of_Elevators_in_Building">Number of Elevators in Building</label></li>
                 
-                <li><label><input type="checkbox" name="shop_Service_Elevators_in_Building">Service Elevators in Building</label></li> 
+                <li><label><input type="checkbox" name="Service_Elevators_in_Building">Service Elevators in Building</label></li> 
                 
-                <li><label><input type="text" name="shop_Other_Main_Features">Other Main Features</label></li>
+                <li><label><input type="text" name="Other_Main_Features">Other Main Features</label></li>
                   
                 
 
@@ -55,11 +55,11 @@
         <div class="col-md-4">
             <h3>Buisness and Communication</h3>
             <ul class="checkboxes inline half list-unstyled">
-                 <li><label><input type="checkbox" name="shop_Broadband_Internet_Access">Broadband Internet Access</label></li>
-                <li><label><input type="checkbox" name="shop_Satellite_or_TV_Cable_Ready">Satellite or TV Cable Ready</label></li>
-                <li><label><input type="checkbox" name="shop_ATM_Credit_Card_Machines_in_Building">ATM Credit Card Machines in Building</label></li>
+                 <li><label><input type="checkbox" name="Broadband_Internet_Access">Broadband Internet Access</label></li>
+                <li><label><input type="checkbox" name="Satellite_or_TV_Cable_Ready">Satellite or TV Cable Ready</label></li>
+                <li><label><input type="checkbox" name="ATM_Credit_Card_Machines_in_Building">ATM Credit Card Machines in Building</label></li>
                 
-                <li><label><input type="text" name="shop_Other_Business_and_Communication_Facilities">Other Business and Communication Facilities</label></li>
+                <li><label><input type="text" name="Other_Business_and_Communication_Facilities">Other Business and Communication Facilities</label></li>
                
             </ul>
             <!--end checkboxes-->
@@ -70,13 +70,13 @@
             <h3>Nearby Locations and Other Facilities</h3>
             <ul class="checkboxes inline half list-unstyled">
                                 
-                <li><label><input type="text" name="shop_Nearby_Schools">Nearby Schools</label></li>
-                <li><label><input type="text" name="shop_Nearby_Hospitals">Nearby Hospitals</label></li>
-                <li><label><input type="text" name="shop_Nearby_Shopping_Malls">Nearby Shopping Malls</label></li>
-                <li><label><input type="text" name="shop_Nearby_Restaurants">Nearby Restaurants</label></li>
-                <li><label><input type="text" name="shop_Distance_From_Airport(kms)">Distance From Airport(kms)</label></li>
-                <li><label><input type="text" name="shop_Nearby_Public_Transport_Service">Nearby Public Transport Service</label></li>
-                <li><label><input type="text" name="shop_Other_Nearby_Places">Other Nearby Places</label></li>
+                <li><label><input type="text" name="Nearby_Schools">Nearby Schools</label></li>
+                <li><label><input type="text" name="Nearby_Hospitals">Nearby Hospitals</label></li>
+                <li><label><input type="text" name="Nearby_Shopping_Malls">Nearby Shopping Malls</label></li>
+                <li><label><input type="text" name="Nearby_Restaurants">Nearby Restaurants</label></li>
+                <li><label><input type="text" name="Distance_From_Airport(kms)">Distance From Airport(kms)</label></li>
+                <li><label><input type="text" name="Nearby_Public_Transport_Service">Nearby Public Transport Service</label></li>
+                <li><label><input type="text" name="Other_Nearby_Places">Other Nearby Places</label></li>
 
             </ul>
             <!--end checkboxes-->
@@ -90,8 +90,8 @@
             <h3>Rooms</h3>
             <ul class="checkboxes inline half list-unstyled">
                 
-                <li><label><input type="text" name="shop_Number_of_Rooms">Number of Rooms</label></li>
-                <li><label><input type="text" name="shop_Other_Rooms">Other Rooms</label></li>
+                <li><label><input type="text" name="Number_of_Rooms">Number of Rooms</label></li>
+                <li><label><input type="text" name="Other_Rooms">Other Rooms</label></li>
                
             </ul>
             <!--end checkboxes-->
@@ -102,10 +102,10 @@
             <h3>Other Facilities</h3>
             <ul class="checkboxes inline half list-unstyled">
                 
-                <li><label><input type="checkbox" name="shop_Maintenance_Staff">Maintenance Staff</label></li>
-                <li><label><input type="checkbox" name="shop_Security_Staff">Security Staff</label></li>
+                <li><label><input type="checkbox" name="Maintenance_Staff">Maintenance Staff</label></li>
+                <li><label><input type="checkbox" name="Security_Staff">Security Staff</label></li>
                 
-                <li><label><input type="text" name="shop_Other_Facilities">Other Facilities</label></li>
+                <li><label><input type="text" name="Other_Facilities">Other Facilities</label></li>
                 
               
             </ul>
