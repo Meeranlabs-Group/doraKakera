7620fc6a859217a5658c1ebcb8d7e4e706e9d913
tayyabafzal95 <tayyabafzal95@gmail.com>
2017-12-04 16:03:59 +0500

    updatee

--- resources/views/features/factory_feature.blade.php
+++ resources/views/features/factory_feature.blade.php
@@ -8,25 +8,25 @@
             <h3>Main Features</h3>
             <ul class="checkboxes inline half list-unstyled">
                 
-                 <li><label><input type="text" name="factory_Built_in_Year">Built in Year</label></li>
-                <li><label><input type="text" name="factory_Parking_Spaces">Parking Spaces</label></li>
+                 <li><label><input type="text" name="Built_in_Year">Built in Year</label></li>
+                <li><label><input type="text" name="Parking_Spaces">Parking Spaces</label></li>
                  
                 <label>Electricity Backup</label>
                 <select>
-                <option value="factory_Electricity_Backup_None">None</option>
-                <option value="factory_Electricity_Backup_Generator">Generator</option>
-                <option value="factory_Electricity_Backup_Ups">Ups</option>
-                <option value="factory_Electricity_Backup_Solar">Solar</option>
-                <option value="factory_Electricity_Backup_Other">Other</option>
+                <option value="Electricity_Backup_None">None</option>
+                <option value="Electricity_Backup_Generator">Generator</option>
+                <option value="Electricity_Backup_Ups">Ups</option>
+                <option value="Electricity_Backup_Solar">Solar</option>
+                <option value="Electricity_Backup_Other">Other</option>
                 </select>
                 
-                <li><label><input type="checkbox" name="factory_Waste_Disposal">Waste Disposal</label></li>
+                <li><label><input type="checkbox" name="Waste_Disposal">Waste Disposal</label></li>
                 
-                <li><label><input type="text" name="factory_Total_Floors_in_Building">Total Floors in Building</label></li>
+                <li><label><input type="text" name="Total_Floors_in_Building">Total Floors in Building</label></li>
                 
-                <li><label><input type="checkbox" name="factory_Elevator_or_Lift">Elevator or Lift</label></li>                
+                <li><label><input type="checkbox" name="Elevator_or_Lift">Elevator or Lift</label></li>                
                 
-                <li><label><input type="text" name="factory_Other_Main_Features">Other Main Features</label></li>
+                <li><label><input type="text" name="Other_Main_Features">Other Main Features</label></li>
                 
 
             </ul>
@@ -36,9 +36,9 @@
         <div class="col-md-4">
             <h3>Buisness and Communication</h3>
             <ul class="checkboxes inline half list-unstyled">
-                <li><label><input type="checkbox" name="factory_Broadband_Internet_Access">Broadband Internet Access</label></li>
-                <li><label><input type="checkbox" name="factory_Satellite_or_TV_Cable_Ready">Satellite or TV Cable Ready</label></li>
-                 <li><label><input type="text" name="factory_Other_Business_and_Communication_Facilities">Other Business and Communication Facilities</label></li>
+                <li><label><input type="checkbox" name="Broadband_Internet_Access">Broadband Internet Access</label></li>
+                <li><label><input type="checkbox" name="Satellite_or_TV_Cable_Ready">Satellite or TV Cable Ready</label></li>
+                 <li><label><input type="text" name="Other_Business_and_Communication_Facilities">Other Business and Communication Facilities</label></li>
                 
                
             </ul>
@@ -48,13 +48,13 @@
         <div class="col-md-4">
             <h3>Nearby Locations and Other Facilities</h3>
             <ul class="checkboxes inline half list-unstyled">
-               <li><label><input type="text" name="factory_Nearby_Schools">Nearby Schools</label></li>
-                <li><label><input type="text" name="factory_Nearby_Hospitals">Nearby Hospitals</label></li>
-                <li><label><input type="text" name="factory_Nearby_Shopping_Malls">Nearby Shopping Malls</label></li>
-                <li><label><input type="text" name="factory_Nearby_Restaurants">Nearby Restaurants</label></li>
-                <li><label><input type="text" name="factory_Distance_From_Airport(kms)">Distance From Airport(kms)</label></li>
-                <li><label><input type="text" name="factory_Nearby_Public_Transport_Service">Nearby Public Transport Service</label></li>
-                <li><label><input type="text" name="factory_Other_Nearby_Places">Other Nearby Places</label></li>
+               <li><label><input type="text" name="Nearby_Schools">Nearby Schools</label></li>
+                <li><label><input type="text" name="Nearby_Hospitals">Nearby Hospitals</label></li>
+                <li><label><input type="text" name="Nearby_Shopping_Malls">Nearby Shopping Malls</label></li>
+                <li><label><input type="text" name="Nearby_Restaurants">Nearby Restaurants</label></li>
+                <li><label><input type="text" name="Distance_From_Airport(kms)">Distance From Airport(kms)</label></li>
+                <li><label><input type="text" name="Nearby_Public_Transport_Service">Nearby Public Transport Service</label></li>
+                <li><label><input type="text" name="Other_Nearby_Places">Other Nearby Places</label></li>
 
             </ul>
             <!--end checkboxes-->
@@ -70,10 +70,10 @@
             <h3>Other Facilities</h3>
             <ul class="checkboxes inline half list-unstyled">
               
-                 <li><label><input type="checkbox" name="factory_Maintenance_Staff">Maintenance Staff</label></li>
-                <li><label><input type="checkbox" name="factory_Security_Staff">Security Staff</label></li>
-                <li><label><input type="checkbox" name="factory_Facilities_for_Disabled">Facilities for Disabled</label></li>
-                 <li><label><input type="text" name="factory_Other_Facilities">Other Facilities</label></li>
+                 <li><label><input type="checkbox" name="Maintenance_Staff">Maintenance Staff</label></li>
+                <li><label><input type="checkbox" name="Security_Staff">Security Staff</label></li>
+                <li><label><input type="checkbox" name="Facilities_for_Disabled">Facilities for Disabled</label></li>
+                 <li><label><input type="text" name="Other_Facilities">Other Facilities</label></li>
                 
               
             </ul>
@@ -83,9 +83,9 @@
             <div class="col-md-4">
             <h3>Healthcare Recreational</h3>
             <ul class="checkboxes inline half list-unstyled">
-                <li><label><input type="checkbox" name="factory_Lawn_or_Garden">Lawn or Garden</label></li>
+                <li><label><input type="checkbox" name="Lawn_or_Garden">Lawn or Garden</label></li>
                 
-                <li><label><input type="text" name="factory_Other_Healthcare_and_Recreation_Facilities">Other Healthcare and Recreation Facilities</label></li>
+                <li><label><input type="text" name="Other_Healthcare_and_Recreation_Facilities">Other Healthcare and Recreation Facilities</label></li>
                 
             </ul>
             <!--end checkboxes-->
