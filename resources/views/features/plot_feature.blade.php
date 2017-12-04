2fc599180e09233b61bc7e7bd4420005a3e99dd7
tayyabafzal95 <tayyabafzal95@gmail.com>
2017-12-04 16:20:44 +0500

    updatee

--- resources/views/features/plot_feature.blade.php
+++ resources/views/features/plot_feature.blade.php
@@ -8,19 +8,19 @@
             <h3>Plot Features</h3>
             <ul class="checkboxes inline half list-unstyled">
                 
-                <li><label><input type="checkbox" name="plot_Possesion">Possesion</label></li>
-                <li><label><input type="checkbox" name="plot_Corner">Corner</label></li>
-                <li><label><input type="checkbox" name="plot_Park_Facing">Park Facing</label></li>
-                <li><label><input type="checkbox" name="plot_Disputed">Disputed</label></li>  
-                <li><label><input type="checkbox" name="plot_File">File</label></li> 
-                <li><label><input type="checkbox" name="plot_Balloted">Balloted</label></li> 
-                <li><label><input type="checkbox" name="plot_Sewerage">Sewerage</label></li> 
-                <li><label><input type="checkbox" name="plot_Electricity">Electricity</label></li> 
-                <li><label><input type="checkbox" name="plot_Water_Supply">Water Supply</label></li> 
-                <li><label><input type="checkbox" name="plot_Sui_Gas">Sui Gas</label></li> 
-                <li><label><input type="checkbox" name="plot_Boundary_Wall">Boundary Wall</label></li> 
+                <li><label><input type="checkbox" name="Possesion">Possesion</label></li>
+                <li><label><input type="checkbox" name="Corner">Corner</label></li>
+                <li><label><input type="checkbox" name="Park_Facing">Park Facing</label></li>
+                <li><label><input type="checkbox" name="Disputed">Disputed</label></li>  
+                <li><label><input type="checkbox" name="File">File</label></li> 
+                <li><label><input type="checkbox" name="Balloted">Balloted</label></li> 
+                <li><label><input type="checkbox" name="Sewerage">Sewerage</label></li> 
+                <li><label><input type="checkbox" name="Electricity">Electricity</label></li> 
+                <li><label><input type="checkbox" name="Water_Supply">Water Supply</label></li> 
+                <li><label><input type="checkbox" name="Sui_Gas">Sui Gas</label></li> 
+                <li><label><input type="checkbox" name="Boundary_Wall">Boundary Wall</label></li> 
                 
-                <li><label><input type="text" name="plot_Other_Plot_Features">Other Plot Features</label></li> 
+                <li><label><input type="text" name="Other_Plot_Features">Other Plot Features</label></li> 
                 
 
             </ul>
@@ -29,13 +29,13 @@
           <div class="col-md-4">
             <h3>Nearby Locations and Other Facilities</h3>
             <ul class="checkboxes inline half list-unstyled">
-                <li><label><input type="text" name="plot_Nearby_Schools">Nearby Schools</label></li>
-                <li><label><input type="text" name="plot_Nearby_Hospitals">Nearby Hospitals</label></li>
-                <li><label><input type="text" name="plot_Nearby_Shopping_Malls">Nearby Shopping Malls</label></li>
-                <li><label><input type="text" name="plot_Nearby_Restaurants">Nearby Restaurants</label></li>
-                <li><label><input type="text" name="plot_Distance_From_Airport(kms)">Distance From Airport(kms)</label></li>
-                <li><label><input type="text" name="plot_Nearby_Public_Transport_Service">Nearby Public Transport Service</label></li>
-                <li><label><input type="text" name="plot_Other_Nearby_Places">Other Nearby Places</label></li>
+                <li><label><input type="text" name="Nearby_Schools">Nearby Schools</label></li>
+                <li><label><input type="text" name="Nearby_Hospitals">Nearby Hospitals</label></li>
+                <li><label><input type="text" name="Nearby_Shopping_Malls">Nearby Shopping Malls</label></li>
+                <li><label><input type="text" name="Nearby_Restaurants">Nearby Restaurants</label></li>
+                <li><label><input type="text" name="Distance_From_Airport(kms)">Distance From Airport(kms)</label></li>
+                <li><label><input type="text" name="Nearby_Public_Transport_Service">Nearby Public Transport Service</label></li>
+                <li><label><input type="text" name="Other_Nearby_Places">Other Nearby Places</label></li>
 
             </ul>
             <!--end checkboxes-->
@@ -50,8 +50,8 @@
             <h3>Other Facilities</h3>
             <ul class="checkboxes inline half list-unstyled">
                 
-                <li><label><input type="checkbox" name="plot_Security_Staff">Security Staff</label></li>
-                <li><label><input type="text" name="plot_Other_Facilities">Other Facilities</label></li>
+                <li><label><input type="checkbox" name="Security_Staff">Security Staff</label></li>
+                <li><label><input type="text" name="Other_Facilities">Other Facilities</label></li>
               
             </ul>
             <!--end checkboxes-->
