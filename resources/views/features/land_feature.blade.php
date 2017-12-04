c77282b200f0377a4f2c28cf744d4d06ea98464a
tayyabafzal95 <tayyabafzal95@gmail.com>
2017-12-04 16:05:45 +0500

    updatee

--- resources/views/features/land_feature.blade.php
+++ resources/views/features/land_feature.blade.php
@@ -8,19 +8,19 @@
             <h3>Plot Features</h3>
             <ul class="checkboxes inline half list-unstyled">
                 
-                <li><label><input type="checkbox" name="land_Possesion">Possesion</label></li>
-                <li><label><input type="checkbox" name="land_Disputed">Disputed</label></li>
-                <li><label><input type="checkbox" name="land_Electricity">Electricity</label></li>
-                <li><label><input type="checkbox" name="land_Sui_Gas">Sui Gas</label></li>  
-                <li><label><input type="checkbox" name="land_Irrigation">Irrigation</label></li> 
-                <li><label><input type="checkbox" name="land_Tube_Wells">Tube Wells</label></li> 
-                <li><label><input type="checkbox" name="land_Accessible_by_Road">Accessible by Road</label></li> 
-                <li><label><input type="checkbox" name="land_Perimeter_Fencing">Perimeter Fencing</label></li> 
-                <li><label><input type="checkbox" name="land_Is_Land_Fertile">Is Land Fertile</label></li> 
-                <li><label><input type="checkbox" name="land_Boundary_Lines">Boundary Lines</label></li> 
+                <li><label><input type="checkbox" name="Possesion">Possesion</label></li>
+                <li><label><input type="checkbox" name="Disputed">Disputed</label></li>
+                <li><label><input type="checkbox" name="Electricity">Electricity</label></li>
+                <li><label><input type="checkbox" name="Sui_Gas">Sui Gas</label></li>  
+                <li><label><input type="checkbox" name="Irrigation">Irrigation</label></li> 
+                <li><label><input type="checkbox" name="Tube_Wells">Tube Wells</label></li> 
+                <li><label><input type="checkbox" name="Accessible_by_Road">Accessible by Road</label></li> 
+                <li><label><input type="checkbox" name="Perimeter_Fencing">Perimeter Fencing</label></li> 
+                <li><label><input type="checkbox" name="Is_Land_Fertile">Is Land Fertile</label></li> 
+                <li><label><input type="checkbox" name="Boundary_Lines">Boundary Lines</label></li> 
                 
-                <li><label><input type="text" name="land_Nearby_Water_Resources">Nearby Water Resources</label></li> 
-                <li><label><input type="text" name="land_Other_Land_Features">Other Land Features</label></li> 
+                <li><label><input type="text" name="Nearby_Water_Resources">Nearby Water Resources</label></li> 
+                <li><label><input type="text" name="Other_Land_Features">Other Land Features</label></li> 
 
             </ul>
             <!--end checkboxes-->
@@ -30,13 +30,13 @@
         <div class="col-md-4">
             <h3>Nearby Locations and Other Facilities</h3>
             <ul class="checkboxes inline half list-unstyled">
-                <li><label><input type="text" name="land_Nearby_Schools">Nearby Schools</label></li>
-                <li><label><input type="text" name="land_Nearby_Hospitals">Nearby Hospitals</label></li>
-                <li><label><input type="text" name="land_Nearby_Shopping_Malls">Nearby Shopping Malls</label></li>
-                <li><label><input type="text" name="land_Nearby_Restaurants">Nearby Restaurants</label></li>
-                <li><label><input type="text" name="land_Distance_From_Airport(kms)">Distance From Airport(kms)</label></li>
-                <li><label><input type="text" name="land_Nearby_Public_Transport_Service">Nearby Public Transport Service</label></li>
-                <li><label><input type="text" name="land_Other_Nearby_Places">Other Nearby Places</label></li>
+                <li><label><input type="text" name="Nearby_Schools">Nearby Schools</label></li>
+                <li><label><input type="text" name="Nearby_Hospitals">Nearby Hospitals</label></li>
+                <li><label><input type="text" name="Nearby_Shopping_Malls">Nearby Shopping Malls</label></li>
+                <li><label><input type="text" name="Nearby_Restaurants">Nearby Restaurants</label></li>
+                <li><label><input type="text" name="Distance_From_Airport(kms)">Distance From Airport(kms)</label></li>
+                <li><label><input type="text" name="Nearby_Public_Transport_Service">Nearby Public Transport Service</label></li>
+                <li><label><input type="text" name="Other_Nearby_Places">Other Nearby Places</label></li>
 
             </ul>
             <!--end checkboxes-->
@@ -50,9 +50,9 @@
             <h3>Other Facilities</h3>
             <ul class="checkboxes inline half list-unstyled">
                 
-                <li><label><input type="checkbox" name="land_Security_Staff">Security Staff</label></li>
+                <li><label><input type="checkbox" name="Security_Staff">Security Staff</label></li>
                 
-                <li><label><input type="text" name="land_Other_Facilities">Other Facilities</label></li> 
+                <li><label><input type="text" name="Other_Facilities">Other Facilities</label></li> 
               
             </ul>
             <!--end checkboxes-->
