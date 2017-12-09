ca7268cdb7114c1fb6d902053c479bfd545aac7f
Afif Rao <afif.rao.9@gmail.com>
2017-12-09 14:51:11 +0500

    user number register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,6 +25,25 @@
                             </div>
                         </div>
 
+
+
+                        <div class="form-group{{ $errors->has('number') ? ' has-error' : '' }}">
+                            <label for="number" class="col-md-4 control-label">Phone Number</label>
+
+                            <div class="col-md-6">
+                                <input id="number" type="tel" class="form-control" name="number" value="{{ old('number') }}" required autofocus>
+
+                                @if ($errors->has('number'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('number') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+
+
+
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
