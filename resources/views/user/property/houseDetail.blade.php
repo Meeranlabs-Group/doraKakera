c1a5b3a176b63ae1b36192e9ef3982036ba801fc
Afif Rao <afif.rao.9@gmail.com>
2018-01-04 13:13:57 +0500

    commite

--- resources/views/user/property/houseDetail.blade.php
+++ resources/views/user/property/houseDetail.blade.php
@@ -41,9 +41,10 @@
                 <div class="main-content">
                     <div class="title">
                         <div class="left">
-                            <h1>Mountain Paradise<span class="rating"><i class="fa fa-star"></i>9.9</span></h1>
-                            <h3><a href="#">Austria</a> (63 properties)</h3>
+                            <h1>{{$data[0]->title}}</h1>
+                            <h3><a href="#">{{$data[0]->block_name}}</a> (63 properties)</h3>
                         </div>
+
                         <div class="right">
                             <a href="#map" class="icon scroll"><i class="fa fa-map-marker"></i>See on the map</a>
                             <a href="t" class="btn btn-primary btn-rounded scroll">Call Now</a>
@@ -52,13 +53,9 @@
                     <!--end title-->
                     <section id="gallery">
                         <div class="gallery-detail">
-                            <div class="ribbon"><div class="offer-number">SALE</div></div>
+                            <div class="ribbon"><div class="offer-number">{{$data[0]->purpose}}</div></div>
 
                             <div class="one-item-carousel">
-
-
-
-
 
                         @foreach($photos as $photo)
                                 <div class="image">
@@ -75,23 +72,15 @@
                     <div class="row">
                         <div class="col-md-8">
                             <section id="description">
-                                <h3>Spend Great Time in Our Hotel!</h3>
-                                <p>Consectetur adipiscing elit. Vivamus nec augue ac dui sodales euismod. Suspendisse
-                                    at dui sit amet felis commodo dictum. Class aptent taciti sociosqu ad litora
-                                    torquent per conubia nostra, per inceptos himenaeos. Integer commodo eleifend erat,
-                                    vitae tincidunt urna volutpat et. Mauris laoreet, sem ut sodales sodales,
-                                    massa turpis posuere lectus, non aliquet massa nisl ac orci.
+                                <h3>{{ $data[0]->price }}</h3>
+                                <p>{{$data[0]->description}}
                                 </p>
-                                <p>Aenean non dapibus neque. Praesent tempus aliquet felis, auctor aliquet ligula bibendum
-                                    id. Phasellus ut finibus ligula. Suspendisse sodales lacus vel viverra egestas. Donec
-                                    eu interdum sem, sed tempus odio. Interdum et malesuada fames ac ante ipsum primis in
-                                    faucibus. In ut ante lacinia, interdum ante eu, posuere ex. Donec iaculis elit
-                                    consectetur nisi finibus, a vestibulum nibh mattis. Aliquam erat volutpat.
-                                </p>
+
                             </section>
                             <section id="facilities">
                                 <h2>Facilities</h2>
                                 <ul class="bullets half">
+
                                     <li>Sauna</li>
                                     <li>Fireplace or fire pit</li>
                                     <li>Outdoor Kitchen</li>
@@ -111,22 +100,7 @@
                         <!--end col-md-8-->
                         <div class="col-md-4">
                             <div class="sidebar">
-                                <aside class="box">
-                                    <dl>
-                                        <dt>1-Bed Rooms:</dt>
-                                        <dd>23</dd>
-                                        <dt>2-Bed Rooms:</dt>
-                                        <dd>48</dd>
-                                        <dt>Apartments:</dt>
-                                        <dd>12</dd>
-                                        <dt>Parking:</dt>
-                                        <dd>Free</dd>
-                                        <dt>Swimming:</dt>
-                                        <dd>Yes</dd>
-                                        <dt>Ski Center:</dt>
-                                        <dd>2km</dd>
-                                    </dl>
-                                </aside>
+
                                 <aside>
                                     <h2>Contact</h2>
                                     <address>
@@ -138,6 +112,25 @@
                                         <a href="#">hello@example.com</a><br>
                                         <strong>skype:</strong> your.company
                                     </address>
+                                </aside>
+
+                                <aside class="box">
+                                    <dl>
+                                        <dt>Bed Rooms:</dt>
+                                        <dd>{{$data[0]->number_of_bedrooms}}</dd>
+                                        <dt>Bath Rooms:</dt>
+                                        <dd>{{$data[0]->number_of_bathrooms}}</dd>
+                                        <dt>Kitchen Rooms:</dt>
+                                        <dd>{{$data[0]->number_of_kitchens}}</dd>
+
+                                        <dt>Area:</dt>
+                                        <dd>{{ $data[0]->unit_size }}: {{ $data[0]->unit_type }}</dd>
+                                        <dt>Parking:</dt>
+                                        <dd>{{ $data[0]->parking_space }}</dd>
+                                        <dt>Swimming:</dt>
+                                        <dd>{{ $data[0]->swimming_pool }}</dd>
+
+                                    </dl>
                                 </aside>
                             </div>
                             <!--end sidebar-->
