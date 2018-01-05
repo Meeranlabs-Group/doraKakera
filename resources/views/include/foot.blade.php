41e9b21def79094fce2086375355d481b97e73eb
Ali Usama <aliusama1@hotmail.com>
2018-01-05 11:27:04 +0500

    new

--- resources/views/include/foot.blade.php
+++ resources/views/include/foot.blade.php
@@ -6,7 +6,7 @@
 
 <script type="text/javascript" src={{ url("assets/js/jquery-2.2.1.min.js")}}></script>
 <script type="text/javascript" src={{ url("assets/js/jquery-migrate-1.2.1.min.js")}}></script>
-<script type="text/javascript" src={{ url("http://maps.google.com/maps/api/js?sensor=false&libraries=places")}}></script>
+<script type="text/javascript" src={{ url("http://maps.google.com/maps/api/js?key=&sensor=false&libraries=places")}}></script>
 <script type="text/javascript" src={{ url("assets/js/infobox.js")}}></script>
 <script type="text/javascript" src={{ url("assets/js/markerclusterer_packed.js")}}></script>
 <script type="text/javascript" src={{ url("assets/js/richmarker-compiled.js")}}></script>
@@ -29,8 +29,6 @@
 <script src={{url("assets/js/prism.js")}}></script>
 <script src={{url("assets/js/jquery.mCustomScrollbar.min.js")}}></script>
 <script src={{url("assets/js/jquery.newsTicker.js")}}></script>
-<script src="http://js.nicedit.com/nicEdit-latest.js" type="text/javascript"></script>
-<script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);</script>
 
 
 <script>
--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -33,12 +33,11 @@
 
 
 
-    <script type="text/javascript" src={{ url("assets/js/ckeditor.js")}}></script>
+
     <link href={{ url("assets/fonts/font-awesome.css") }} rel="stylesheet" type="text/css">
     <link href={{ url("assets/fonts/elegant-fonts.css") }} rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700,900,400italic' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href={{ url("assets/bootstrap/css/bootstrap.css") }} type="text/css">
-    <link rel="stylesheet" href={{ url("assets/css/zabuto_calendar.min.css") }} type="text/css">
     <link rel="stylesheet" href={{ url("assets/css/owl.carousel.css") }} type="text/css">
     <link rel="stylesheet" href={{ url("assets/css/style.css") }} type="text/css">
 
